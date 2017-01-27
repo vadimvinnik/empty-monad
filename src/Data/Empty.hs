1904fa430daf1801047c111d8c15d838dee53342
Vadim Vinnik <vadim.vinnik@gmail.com>
2017-01-27 00:38:46 +0000

    New class instances. Documentation

--- src/Data/Empty.hs
+++ src/Data/Empty.hs
@@ -1,17 +1,37 @@
+{- |
+
+Module      :  Data.Empty
+Copyright   :  Vadim Vinnik, 2017
+License     :  BSD-style (see the LICENSE file in the distribution)
+
+Maintainer  :  vadim.vinnik@gmail.com
+Stability   :  experimental
+Portability :  portable
+
+An always empty data structure, i.e. a degenerate container that contains no
+elements but is aware of the (missing) elements' type,  and is a first-class
+citizen of a number of type classes.  E.g. 'fmap'ping of any function over
+'Empty' is also 'Empty', two 'Empty' container can be 'mappend'ed that gives
+'Empty' again etc.
+
+-}
+
 module Data.Empty (
-  Empty,
+  Empty(..),
   unwrap
 ) where
 
 import Control.Applicative (Alternative(..))
 import Control.Monad (MonadPlus(..))
 
+-- | the empty container
 data Empty a = Empty
 
+-- | obtain the (missing) value from the container -- always fails
 unwrap :: Empty a -> a
--- not a temporary placeholder, this is the desired and correct implementation
-unwrap = undefined
+unwrap = undefined -- not a stub but the desired correct implementation
 
+-- for internal use: creates a const function of 2 arguments
 const2 :: a -> b -> c -> a
 const2 = const . const
 
@@ -25,8 +45,18 @@ instance Monoid (Empty a) where
   mempty = Empty
   mappend = const2 Empty
 
+instance Foldable Empty where
+  foldr _ = const
+  foldl _ = const
+  null = const False
+  length = const 0
+  elem = const2 False
+
 instance Functor Empty where
   fmap _ e = Empty
+
+instance Traversable Empty where
+  traverse _ Empty = pure Empty
 
 instance Applicative Empty where
   pure = const Empty
